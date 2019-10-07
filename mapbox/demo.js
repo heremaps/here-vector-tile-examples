55e8c1fad074b801d20fadabe289f507b7c656d6
Francisco Sanmartin <francisco.sanmartin-gomez@here.com>
2019-10-07 16:50:18 +0200

    Mapbox demo: removed inline css, changed to window.apikey, added headers and info about demo

--- mapbox/demo.js
+++ mapbox/demo.js
@@ -1,6 +1,6 @@
 const map = new mapboxgl.Map({
    container: 'map',
-   style: 'https://assets.vector.hereapi.com/styles/berlin/base/mapbox/tilezen?apikey=' + apikey,
+   style: 'https://assets.vector.hereapi.com/styles/berlin/base/mapbox/tilezen?apikey=' + window.apikey,
    center: [-122.431297, 37.773972], //Longitude, Latitude
    zoom: 11
 });
